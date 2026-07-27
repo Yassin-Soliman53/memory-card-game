ad8a1558b86d204953121c5c1af77579745644ba
Yassin Soliman <yassin.soliman.120@gmail.com>
2026-07-27 16:47:07 +0300

    Your descriptive message here

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,7 +5,7 @@ function Nav() {
   return (
     <nav className="nav">
       <div className="nav-div">
-        <div className="logo"> <a href=""></a></div>
+        <div className="logo"> <img src="{logo}"></img></div>
         <ul className="nav-ul">
           <li className="nav-li"><Link to="/">Home</Link></li>
           <li className="nav-li"><Link to="/GamePage">Play Game</Link></li>
