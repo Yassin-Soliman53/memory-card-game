036b612fb5b77e1034cd392e58915d5c40f061e0
Yassin Soliman <yassin.soliman.120@gmail.com>
2026-07-27 23:14:42 +0300

    Your descriptive commit message here

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/memory-card-game/', // <-- Add this line
   plugins: [react()],
+  base: '/memory-card-game/', // Must match your repo name exactly
 })
